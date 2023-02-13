bc30ea875500a90d7b4957a58461d9a680dd9313
sathyark652 <117423140+sathyark652@users.noreply.github.com>
2023-02-13 18:56:08 +0530

    Create new.cpp

---
+++ new.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "New file\n";
+  cout << "Srn:PES2UG20CS315\n";
+  cout << "I have successfully built and run! \n";
+  return 0;
+}
