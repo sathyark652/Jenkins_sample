b25543b499f34273e955df6c4621dc989a17e59f
sathyark652 <117423140+sathyark652@users.noreply.github.com>
2023-02-13 18:26:48 +0530

    Create PES2UG20CS315.cpp

---
+++ PES2UG20CS315.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "New file\n";
+  cout << "PES2UG20CS315\n";
+  cout << "I have successfully built and run! \n";
+  return 0;
+}
